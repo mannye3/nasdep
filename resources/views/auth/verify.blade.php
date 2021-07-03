ee3aba1cfa738da23b2358948f6a5b52d762c543
unknown <aboajahemmanuel@gmail.com>
2021-07-03 21:17:19 +0100

    update

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
+
+
+
+
+<section class="shop-checkouts shop-order">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="order_complete_message text-center">
+                    <div class="icon "><span class="fa fa-envelope" style="font-size: 100px"></span></div>
+                    <h2>Verify Your Email Address</h2>
                     @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
+                @endif
+                    <p>Before proceeding, please check your email for a verification link. If you did not receive the email, </p>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit"  class=" btn-thm">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
         </div>
+
     </div>
-</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::prefix('author')->group(function(){
     // Route::get('comments', 'AuthorController@comments')->name('authorComments');
     // Route::get('posts/new', 'AuthorController@newPost')->name('newPost');
     // Route::post('posts/new', 'AuthorController@createPost')->name('createPost');
-    // Route::get('post/{id}/edit', 'AuthorController@postEdit')->name('postEdit');
+    // Route:.:get('post/{id}/edit', 'AuthorController@postEdit')->name('postEdit');
     // Route::post('post/{id}/edit', 'AuthorController@postEditPost')->name('postEditPost');
     // Route::get('post/{id}/delete', 'AuthorController@deletePost')->name('deletePost');
     Route::get('posts', 'AuthorController@posts')->name('authorPosts');
